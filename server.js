2f6373ed2c903242b459c2d6d3aa3dff66bb9d39
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-24 16:11:29 -0500

    Added index route for /

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,6 +24,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
